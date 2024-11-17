3582dc3b5f81db5ecc6ac18bb3b2785027128be4
Akhmanov Aleksei <alexboost222@gmail.com>
2024-11-17 22:02:40 +0400

    16! added code generation based on table data

---
+++ C4G.Unity/Assets/C4G/Editor/Scripts/CodeGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace C4G.Editor
+{
+    public static class CodeGenerator
+    {
+        public static string GenerateDTOClass(ParsedSheet parsedSheet)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"public sealed class {parsedSheet.Name}");
+            sb.AppendLine("{");
+
+            foreach (var property in parsedSheet.Properties)
+            {
+                sb.AppendLine($"    public {property.Type} {property.Name} {{ get; set; }}");
+            }
+
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+        public static string GenerateWrapperClass(ParsedSheet parsedSheet)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("using System.Collections.Generic;");
+            sb.AppendLine();
+            sb.AppendLine($"public sealed class {parsedSheet.Name}Wrapper");
+            sb.AppendLine("{");
+            
+            sb.AppendLine("    public string Name { get; set; }");
+            sb.AppendLine($"    public List<{parsedSheet.Name}> Entities {{ get; set; }} = new List<{parsedSheet.Name}>();");
+
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+    }
+}
