e594fc0257aad014a620b7991f743d8c9bed47f0
johnn <johnnyzaev@gmail.com>
2025-08-25 20:49:17 +0400

    Remove generated files and update .gitignore

--- C4G.Unity/Assets/Scripts/Generated/Sheet4.cs
+++
@@ -1,7 +0,0 @@
-public partial class Sheet4
-{
-    public int Id { get; set; }
-    public int BaseHp { get; set; }
-    public float BaseDamage { get; set; }
-    public string Name { get; set; }
-}
--- C4G.Unity/Assets/Scripts/Generated/Sheet4Wrapper.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-public partial class Sheet4Wrapper
-{
-    public string Name { get; set; }
-    public List<Sheet4> Entities { get; set; } = new List<Sheet4>();
-}
