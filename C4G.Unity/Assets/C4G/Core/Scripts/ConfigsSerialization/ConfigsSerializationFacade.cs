c52ba33133a1928096bd8579ef5ac2467b599853
Akhmanov Aleksei <alexboost222@gmail.com>
2025-09-16 22:11:27 +0400

    29 reworked previously implemented simple types serialization added list support, made it easy to support another simple and collection types in future, added positive, negative and destructive test cases for lists and simple types to ConfigsSerializationFacadeTests.cs

--- C4G.Unity/Assets/C4G/Core/Scripts/ConfigsSerialization/ConfigsSerializationFacade.cs
+++ C4G.Unity/Assets/C4G/Core/Scripts/ConfigsSerialization/ConfigsSerializationFacade.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 using C4G.Core.SheetsParsing;
 using C4G.Core.Utils;
 using Newtonsoft.Json;
@@ -14,25 +17,123 @@ namespace C4G.Core
             if (!isValid)
                 return Result<string, string>.FromError(error);
 
-            var jsonDto = new JsonDto(parsedSheet.Name, new List<Dictionary<string, string>>());
+            var jsonDto = new JsonDto(parsedSheet.Name, new List<IReadOnlyDictionary<string, object>>());
 
             foreach (IReadOnlyCollection<string> entityData in parsedSheet.Entities)
             {
-                var entityDataDict = new Dictionary<string, string>();
-
-                int index = 0;
-                foreach (var property in parsedSheet.Properties)
-                {
-                    entityDataDict[property.Name] = entityData.ElementAt(index);
-                    index++;
-                }
-
-                jsonDto.Entities.Add(entityDataDict);
+                Result<IReadOnlyDictionary<string, object>, string> entityDataDictResult = GetEntityDataDict(entityData, parsedSheet.Properties);
+                if (!entityDataDictResult.IsOk)
+                    return Result<string, string>.FromError(entityDataDictResult.Error);
+                jsonDto.Entities.Add(entityDataDictResult.Value);
             }
 
             string json = JsonConvert.SerializeObject(jsonDto, Formatting.Indented);
 
             return Result<string, string>.FromValue(json);
+        }
+
+        private static Result<IReadOnlyDictionary<string, object>, string> GetEntityDataDict(
+            IReadOnlyCollection<string> entityData, IReadOnlyList<ParsedPropertyInfo> properties)
+        {
+            var entityDataDict = new Dictionary<string, object>();
+
+            int index = 0;
+
+            foreach (ParsedPropertyInfo property in properties)
+            {
+                string serializedPropertyValue = entityData.ElementAt(index);
+                Result<object, string> propertyValueResult = GetPropertyValue(property, serializedPropertyValue);
+                if (!propertyValueResult.IsOk)
+                    return Result<IReadOnlyDictionary<string, object>, string>.FromError(propertyValueResult.Error);
+                    
+                entityDataDict[property.Name] = propertyValueResult.Value;
+                index++;
+            }
+
+            return Result<IReadOnlyDictionary<string, object>, string>.FromValue(entityDataDict);
+        }
+
+        private static readonly Dictionary<string, Func<string, Result<object, string>>> SimpleTypeParsers
+            = new Dictionary<string, Func<string, Result<object, string>>>
+        {
+            { "int", TryParseFuncResultDecorator<int>(int.TryParse) },
+            { "float", TryParseFuncResultDecorator((string s, out float f) => float.TryParse(s, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out f)) },
+            { "double", TryParseFuncResultDecorator((string s, out double d) => double.TryParse(s, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out d)) },
+            { "bool", TryParseFuncResultDecorator<bool>(bool.TryParse) },
+            { "string", Result<object, string>.FromValue }
+        };
+
+        private static readonly List<(Regex typePattern, char separator)> CollectionParsers = new List<(Regex typePattern, char separator)> 
+        {
+            (new Regex("^List<(.+)>$", RegexOptions.Compiled | RegexOptions.IgnoreCase), ',') 
+        };
+
+        private delegate bool TryParseFunc<T>(string serializedValue, out T value);
+
+        private static Func<string, Result<object, string>> TryParseFuncResultDecorator<T>(TryParseFunc<T> tryParseFunc)
+        {
+            return serializedValue => tryParseFunc(serializedValue, out T value)
+                ? Result<object, string>.FromValue(value)
+                : Result<object, string>.FromError($"Could not parse property '{serializedValue}' as {typeof(T).Name}");
+        }
+
+        private static Result<object, string> GetPropertyValue(ParsedPropertyInfo property, string serializedPropertyValue)
+        {
+            if (SimpleTypeParsers.TryGetValue(property.Type, out Func<string, Result<object, string>> simpleTypeParser))
+            {
+                return simpleTypeParser(serializedPropertyValue);
+            }
+
+            foreach ((Regex typePattern, char separator) in CollectionParsers)
+            {
+                Match match = typePattern.Match(property.Type);
+
+                if (match.Success)
+                {
+                    if (match.Length != property.Type.Length)
+                        return Result<object, string>.FromError($"Collection parser regex pattern '{typePattern}' matches only part '{match.Value}' of property type '{property.Type}', but should only match whole type");
+
+                    if (match.Groups.Count != 2)
+                        return Result<object, string>.FromError($"Collection parser regex pattern '{typePattern}' captures '{match.Groups.Count}' groups but should capture only 2 - Collection pattern itself and collection type");
+
+                    string elementType = match.Groups[1].Value;
+
+                    return ParseList(serializedPropertyValue, elementType, separator);
+                }
+            }
+
+            return Result<object, string>.FromError($"Cannot parse property with type '{property.Type}'");
+        }
+
+        private static Result<object, string> ParseList(string serializedList, string elementType, char separator)
+        {
+            if (string.IsNullOrWhiteSpace(serializedList))
+                return Result<object, string>.FromValue(new List<object>());
+
+            if (!SimpleTypeParsers.TryGetValue(elementType, out Func<string, Result<object, string>> simpleTypeParser))
+                return Result<object, string>.FromError($"Cannot parse list elements type '{elementType}'");
+
+            var result = new List<object>();
+            string[] serializedElements = serializedList.Split(separator);
+
+            foreach (string serializedElement in serializedElements)
+            {
+                string trimmedSerializedElement = serializedElement.Trim();
+                if (string.IsNullOrEmpty(trimmedSerializedElement))
+                    return Result<object, string>.FromError($"Cannot parse empty element in list '{serializedList}' with type '{elementType}'");
+
+                Result<object, string> elementParseResult = simpleTypeParser(trimmedSerializedElement);
+
+                if (!elementParseResult.IsOk)
+                {
+                    return Result<object, string>.FromError($"Cannot parse collection element '{serializedElement}' as {elementType}\n" +
+                                                            $"Inner error: {elementParseResult.Error}");
+                }
+
+                result.Add(elementParseResult.Value);
+            }
+
+            return Result<object, string>.FromValue(result);
         }
 
         private static bool ValidateParsedSheet(ParsedSheet parsedSheet, out string error)
@@ -80,9 +181,9 @@ namespace C4G.Core
             internal string Name { get; }
 
             [JsonProperty("entities")]
-            internal List<Dictionary<string, string>> Entities { get; }
+            internal List<IReadOnlyDictionary<string, object>> Entities { get; }
 
-            internal JsonDto(string name, List<Dictionary<string, string>> entities)
+            internal JsonDto(string name, List<IReadOnlyDictionary<string, object>> entities)
             {
                 Name = name;
                 Entities = entities;
