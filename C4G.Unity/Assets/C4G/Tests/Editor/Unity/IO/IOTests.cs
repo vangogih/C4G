33c8891a98a9c057eb0d550583e666027e232c5d
vangogih <vangogih@yandex.ru>
2026-03-30 17:51:09 +0200

    one failed OS specific test fixed

--- C4G.Unity/Assets/C4G/Tests/Editor/Unity/IO/IOTests.cs
+++ C4G.Unity/Assets/C4G/Tests/Editor/Unity/IO/IOTests.cs
@@ -52,7 +52,7 @@ namespace C4G.Tests.Editor.Unity.IO
 		[Test]
 		public void WriteToFile_InvalidPath_ReturnsError()
 		{
-			string invalidFolder = _tempRoot + Path.DirectorySeparatorChar + "::??**";
+			string invalidFolder = _tempRoot + Path.DirectorySeparatorChar + "inv\0lid";
 
 			Result<string> result = _io.WriteToFile(invalidFolder, "f.txt", "x");
 
